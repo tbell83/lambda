fe493d1995ee0bbad289b83d9ed25b63497db6a1
Thomas Bell <tbell@dramafever.com>
2018-04-13 15:26:21 -0400

    cant count outputs

--- output.tf
+++ output.tf
@@ -1,9 +1,7 @@
 output "lambda_invoke_arn" {
-  count = "${var.count}"
   value = "${aws_lambda_function.lambda.*.invoke_arn}"
 }
 
 output "lambda_arn" {
-  count = "${var.count}"
   value = "${aws_lambda_function.lambda.*.arn}"
 }
