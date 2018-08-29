d7fb9a44647a3ab8daf044d788bc44402f71dd7e
Tom Bell <tbell@dramafever.com>
2018-08-29 16:44:53 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
 output "lambda_invoke_arn" {
-  value = "${aws_lambda_function.lambda.*.invoke_arn}"
+  value = "${var.edge == "true" ? aws_lambda_function.lambda_edge.*.invoke_arn : aws_lambda_function.lambda.*.invoke_arn}"
 }
 
 output "lambda_arn" {
-  value = "${aws_lambda_function.lambda.*.arn}"
+  value = "${var.edge == "true" ? aws_lambda_function.lambda_edge.*.arn : aws_lambda_function.lambda.*.arn}"
 }
 
 output "lambda_role_arn" {
