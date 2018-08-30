b36ab3cabadd8524fc6c0337bcdefc8239b346c8
Tom Bell <tbell@dramafever.com>
2018-08-30 09:10:13 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  lambda_arn = "${var.edge == "true" ? aws_lambda_function.lambda_edge.*.arn : aws_lambda_function.lambda.*.arn}"
-  lambda_invoke_arn = "${var.edge == "true" ? aws_lambda_function.lambda_edge.*.invoke_arn : aws_lambda_function.lambda.*.invoke_arn}"
+  lambda_arn = "${split(",", var.edge == "true" ? join(",", aws_lambda_function.lambda_edge.*.arn) : join(",", aws_lambda_function.lambda.*.arn))}"
+  lambda_invoke_arn = "${split(",", var.edge == "true" ? join(",", aws_lambda_function.lambda_edge.*.invoke_arn) : join(",", aws_lambda_function.lambda.*.invoke_arn))}"
   lambda_role_arn = "${aws_iam_role.lambda.*.arn}"
 }
