dd266ae0f7f9a58ef6baf431d2751149d03c77d4
Thomas Bell <tbell@dramafever.com>
2019-03-15 09:35:27 -0400

    add role name output

--- locals.tf
+++ locals.tf
@@ -1,5 +1,6 @@
 locals {
-  lambda_arn = "${split(",", var.edge == "true" ? join(",", aws_lambda_function.lambda_edge.*.arn) : join(",", aws_lambda_function.lambda.*.arn))}"
+  lambda_arn        = "${split(",", var.edge == "true" ? join(",", aws_lambda_function.lambda_edge.*.arn) : join(",", aws_lambda_function.lambda.*.arn))}"
   lambda_invoke_arn = "${split(",", var.edge == "true" ? join(",", aws_lambda_function.lambda_edge.*.invoke_arn) : join(",", aws_lambda_function.lambda.*.invoke_arn))}"
-  lambda_role_arn = "${aws_iam_role.lambda.*.arn}"
+  lambda_role_arn   = "${aws_iam_role.lambda.*.arn}"
+  lambda_role_name  = "${aws_iam_role.lambda.*.name}"
 }
--- output.tf
+++ output.tf
@@ -9,3 +9,7 @@ output "lambda_arn" {
 output "lambda_role_arn" {
   value = "${local.lambda_role_arn}"
 }
+
+output "lambda_role_name" {
+  value = "${local.lambda_role_name}"
+}
