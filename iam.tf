4783b2d603d75b14ec2b22d05343d749f962dbde
Thomas Bell <tbell@dramafever.com>
2019-08-09 11:37:42 -0400

    length

--- iam.tf
+++ iam.tf
@@ -23,7 +23,7 @@ data "aws_iam_policy_document" "assume_role" {
     }
 
     dynamic "principals" {
-      for_each = len(var.assume_principals)
+      for_each = length(var.assume_principals)
       content {
         type        = "AWS"
         identifiers = var.assume_principals
